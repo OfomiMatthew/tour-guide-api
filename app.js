8afa86862376c3e13db11a9f78ee6f590632d068
MatthewOfomi <ofomimatthew7@gmail.com>
2024-02-23 16:03:40 +0100

    create tours endpoint

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const fs = require("fs");
 
 const app = express();
+app.use(express.json());
 
 const PORT = 6000;
 
@@ -9,9 +10,28 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/devData/data/tours.json`)
 );
 
+// endpoint to get all tours
 app.get("/api/v1/tours/", (req, res) => {
-  res.json({ status: "success", data: { tours } });
+  res.json({ status: "success", results: tours.length, data: { tours } });
 });
+
+// endpoint to create new tours
+app.post("/api/v1/tours", (req, res) => {
+const newId = tours[tours.length-1].id +1
+const newTour = Object.assign({id:newId},req.body)
+tours.push(newTour)
+fs.writeFile(`${__dirname}/devData/data/tours.json`,JSON.stringify(tours),(err)=>{
+res.status(201).json({
+  status:"success",
+  data:{
+    tour:newTour
+  }
+})
+})
+
+
+});
+
 
 app.listen(PORT, () => {
   console.log(`listening to port ${PORT}`);
